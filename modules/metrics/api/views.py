e6da6dd10ad514387bcb3711e3b17d4d28c6fcb6
fr0mhell <t1m0feev.grigorij@gmail.com>
2022-02-10 16:45:00 +0700

    WIP improve aggregation in the API

--- modules/metrics/api/views.py
+++ modules/metrics/api/views.py
@@ -1,6 +1,5 @@
 from rest_framework import mixins, viewsets
 from rest_framework.response import Response
-from typing import Set
 from ..models import Metric
 from .serializers import MetricSerializer
 from rest_framework.filters import OrderingFilter
@@ -19,6 +18,8 @@ class MetricViewSet(
         OrderingFilter,
     )
     filterset_fields = (
+        # TODO add comparison for dates
+        'date',
         'channel',
         'country',
         'os',
@@ -28,50 +29,49 @@ class MetricViewSet(
     ordering_fields = '__all__'
 
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-
-        group_by_values = self.get_group_by_values(queryset)
-        if group_by_values:
-            queryset = queryset.values(*group_by_values)
-
-        queryset = self.filter_queryset(queryset)
-
-        columns = self.get_columns()
-        if columns:
-            queryset = queryset.annotate(**columns)
-
+        queryset = self.filter_queryset(self.get_queryset())
+        queryset = self.aggregate_data(queryset)
         page = self.paginate_queryset(queryset)
 
         if page:
-            if group_by_values:
-                data = page
-            else:
-                serializer = self.get_serializer(page, many=True)
-                data = serializer.data
+            data = self.serialize_data(page)
             return self.get_paginated_response(data)
 
-        if group_by_values:
-            data = queryset
-        else:
-            serializer = self.get_serializer(page, many=True)
-            data = serializer.data
+        data = self.serialize_data(queryset)
         return Response(data)
 
-    def get_group_by_values(self, queryset):
-        raw_columns = self.request.query_params.get('group_by', '')
-        if not raw_columns:
-            return
+    @property
+    def is_aggregation(self):
+        group_by = self.request.query_params.get('group_by', '')
+        columns = self.request.query_params.get('columns', '')
+        return bool(group_by and columns)
 
-        columns = raw_columns.split(',')
-        return columns
+    def aggregate_data(self, queryset):
+        """Aggregate data.
 
-    def get_columns(self):
-        fields = self.request.query_params.get('columns', '')
-        if not fields:
-            return
 
-        columns = {column: Sum(column) for column in fields.split(',')}
-        return columns
+
+        """
+        if not self.is_aggregation:
+            return queryset
+
+        group_by = self.request.query_params.get('group_by').split(',')
+        columns = self.request.query_params.get('columns').split(',')
+        aggregated_columns = {column: Sum(column) for column in columns}
+        queryset = queryset.values(*group_by).annotate(**aggregated_columns)
+        return queryset
+
+    def serialize_data(self, queryset):
+        """Serialize data for response.
+
+
+
+        """
+        if self.is_aggregation:
+            return queryset
+
+        serializer = self.get_serializer(queryset, many=True)
+        return serializer.data
 
 
 """
